0eae021f73ba615d76d28ba52b18c04188c22a65
Felipe Afonso <fmunhozafonso@gmail.com>
2024-07-21 02:17:22 -0300

    feat: finished new content for projects

--- content/projects.ts
+++ content/projects.ts
@@ -12,6 +12,13 @@ import Alura1 from "./images/AluraC-Community.png";
 import Alura2 from "./images/AluraC-Editor.png";
 import Alura3 from "./images/AluraCMobile-Community.png";
 import Alura4 from "./images/AluraCMobile-Editor.png";
+import HTMXGleam from "./images/HTMX_Gleam.png";
+import HomeOptions from "./images/HomeOptions.png";
+import HomeOptionsBlog from "./images/HomeOptionsBlog.png";
+import Liftup from "./images/LiftUpPortal.png";
+import LiftupCustomer from "./images/LiftUpPortalCustomer.png";
+import StupidSpiderGameIG from "./images/StupidSpiderGameIG.png";
+import StupidSpiderGameTitle from "./images/StupidSpiderGameTitle.png";
 
 import type { ImageMetadata } from "astro";
 
@@ -24,6 +31,33 @@ export type Project = {
 };
 
 export const projects: Record<string, Project> = {
+  stupid_spooder_game: {
+    title: "Stupid Spooder Game",
+    description:
+      "I crafted a simple game using Rust and the Bevy Game Engine. It may be a small undertaking, but I developed everything, from sprites and music to interactions.",
+    link: "https://king-cod.itch.io/stupid-spooder-game",
+    images: [StupidSpiderGameTitle, StupidSpiderGameIG],
+  },
+  gleam_htmx: {
+    title: "HTMX Tailwind Gleam",
+    description:
+      "I created an HTMX-driven web server for the functional programming Gleam language. This open-source project is a code template with built-in tailwind support, hot reloading, and componentized templating.",
+    link: "https://github.com/FelipeAfonso/gleam_htmx_tw",
+    images: [HTMXGleam],
+  },
+  liftup: {
+    title: "Liftup Public Portals",
+    description:
+      "I created and maintained all of Liftup's public portals, including numerous forms to channel customer requests, landing pages, ad campaign entry points, and more!",
+    images: [Liftup, LiftupCustomer],
+  },
+
+  homeoptions: {
+    title: "HomeOptions Public Portals",
+    images: [HomeOptions, HomeOptionsBlog],
+    description: `HomeOptions is a real estate platform that facilitates connections between buyers and sellers. I was involved in developing public portals for users to submit requests for financial products, access blog posts, and contact real estate agents.`,
+  },
+
   flora: {
     title: "AG Platform Flora",
     description: `Flora, the internal name of Santos Lab's main software, is a unique web platform for farmers and agronomists. It offers a novel approach to exploring data from different origins within their properties. With its intricate web maps, unique charts on the dashboard, numerous forms, and innovative components, Flora stands out in the agricultural tech landscape.`,
