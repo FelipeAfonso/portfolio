f58eb2678d97362ebbff0801596ca45e08497547
Felipe Afonso <fmunhozafonso@gmail.com>
2025-05-05 13:19:51 -0300

    feat: added seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
+import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), mdx()],
+  integrations: [tailwind(), alpinejs(), sitemap(), mdx()],
+  site: "https://www.felipeafonso.com",
 });
